dbc140763729f9d2ec7a48b472671c6d492edede
Joel Elizaga <joelizaga@hotmail.com>
2016-05-19 13:17:44 -0700

    -

---
+++ iKid/iKid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Joel Elizaga on 5/19/16.
+//  Copyright © 2016 Joel Elizaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
